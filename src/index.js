43d5cd83debd3303fcad7e39c8d243355a4074fb
bajmutov <bajmutov@gmail.com>
2023-07-29 12:56:48 +0300

    init

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import axios from 'axios';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
