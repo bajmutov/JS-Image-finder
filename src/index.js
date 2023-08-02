7fbece3bc587b92409a6652f101ca92a5d1f02ed
Oleksandr <bajmutov@gmail.com>
2023-08-02 19:18:37 +0300

    first

--- src/index.js
+++ src/index.js
@@ -1,5 +1,99 @@
-import axios from 'axios';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import { PixabayAPI } from './js/api';
 
-import SimpleLightbox from "simplelightbox";
-import "simplelightbox/dist/simple-lightbox.min.css";
+const searchForm = document.querySelector('.search-form');
+const loadMoreBtn = document.querySelector('.load-more');
+const listCardsEl = document.querySelector('.gallery');
+
+const newsApiService = new PixabayAPI();
+
+searchForm.addEventListener('submit', handleSearchArticles);
+loadMoreBtn.addEventListener('click', searchMoreArticles);
+
+loadMoreBtn.classList.add('is-hidden');
+
+function handleSearchArticles(event) {
+  event.preventDefault();
+
+  loadMoreBtn.classList.add('is-hidden');
+  newsApiService.query = event.currentTarget.elements.searchQuery.value;
+
+  newsApiService.resetPage();
+  clearGalleryContainer();
+
+  const searchImg = newsApiService.query.trim();
+  if (!searchImg) {
+    return Notify.failure(`❌Еnter a word to search for❌`);
+  }
+
+  newsApiService
+    .fetchPosts()
+    .then(response => {
+      if (!response.hits.length) {
+        throw new Error(
+          'Sorry, there are no images matching your search query. Please try again.'
+        );
+      }
+      Notify.success(`Hooray! We found ${response.totalHits} images.`);
+      newsApiService.incrementPage();
+      createGalleryCards(response.hits);
+
+      loadMoreBtn.classList.remove('is-hidden');
+    })
+    .catch(err => {
+      Notify.failure(`❌${err.message}❌`);
+    });
+}
+
+function searchMoreArticles() {
+  newsApiService
+    .fetchPosts()
+    .then(response => {
+      const numb = newsApiService.multiplyPages();
+      if (response.totalHits <= numb) {
+        loadMoreBtn.classList.add('is-hidden');
+      }
+
+      newsApiService.incrementPage();
+      createGalleryCards(response.hits);
+    })
+    .catch(err => {
+      Notify.failure(`❌${err.message}❌`);
+    });
+
+  //   loadMoreBtn.disable();
+  //   newsApiService.fetchArticles().then(articles => {
+  //     appendArticlesMarkup(articles);
+  //     loadMoreBtn.enable();
+}
+
+function createGalleryCards(images) {
+  const markup = images
+    .map(hits => {
+      return `<div class="photo-card">
+  <img src="${hits.webformatURL}" alt="${hits.tags}" loading="lazy" width="360" height="240" />
+  <div class="info">
+    <p class="info-item">
+      <b>Likes</b> ${hits.likes}
+    </p>
+    <p class="info-item">
+      <b>Views</b> ${hits.views}
+    </p>
+    <p class="info-item">
+      <b>Comments</b> ${hits.comments}
+    </p>
+    <p class="info-item">
+      <b>Downloads</b> ${hits.downloads}
+    </p>
+  </div>
+</div>`;
+    })
+    .join('');
+  listCardsEl.insertAdjacentHTML('beforeend', markup);
+}
+
+function clearGalleryContainer() {
+  listCardsEl.innerHTML = '';
+}
---
+++ src/js/api.js
@@ -0,0 +1,58 @@
+import axios from 'axios';
+
+export class PixabayAPI {
+  #BASE_URL = 'https://pixabay.com/api/';
+  #API_KEY = '38604486-c97a7af17c6668551d7e0c5c6';
+
+  constructor() {
+    this.searchQuery = '';
+    this.page = 1;
+    this.perPageImg = 3;
+    this.typeImage = 'photo';
+    this.orientationImg = 'horizontal';
+    this.safeSearchAge = true;
+  }
+
+  multiplyPages() {
+    return this.page * this.perPageImg;
+  }
+
+  async fetchPosts() {
+    // console.log(this);
+    const searchParams = new URLSearchParams({
+      key: this.#API_KEY,
+      q: this.searchQuery,
+      image_type: this.typeImage,
+      orientation: this.orientationImg,
+      safesearch: this.safeSearchAge,
+      page: this.page,
+      per_page: this.perPageImg,
+    });
+
+    const response = await axios
+      .get(`${this.#BASE_URL}?${searchParams}`)
+      .then(({ data }) => {
+        console.log(data);
+        // this.incrementPage();
+        return data;
+      });
+
+    return response;
+  }
+
+  incrementPage() {
+    this.page += 1;
+  }
+
+  resetPage() {
+    this.page = 1;
+  }
+
+  get query() {
+    return this.searchQuery;
+  }
+
+  set query(newQuery) {
+    this.searchQuery = newQuery;
+  }
+}
